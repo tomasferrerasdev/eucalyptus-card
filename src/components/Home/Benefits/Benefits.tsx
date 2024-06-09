4691339dab9547e37f06225db3dab03957a2a275
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-09 15:50:09 -0300

    feat: add benefits parallax and title

--- src/components/Home/Benefits/Benefits.tsx
+++ src/components/Home/Benefits/Benefits.tsx
@@ -1,3 +1,24 @@
-export const Benefits = () => {
-  return <div>Benefits</div>;
+import { ParallaxSection } from '@/components/Layout';
+import styles from './Benefits.module.scss';
+
+type Benefit = {
+  title: string;
+  href: string;
+};
+
+interface BenefitsProps {
+  benefits: Benefit[];
+}
+
+export const Benefits = ({ benefits }: BenefitsProps) => {
+  return (
+    <ParallaxSection parallaxValues={[10, 20]}>
+      <section className={styles.benefitsWrapper}>
+        <div className={`${styles.titleContainer} container`}>
+          <h2 className="h2">With MINT, </h2>
+          <h2 className="h2">it really adds up.</h2>
+        </div>
+      </section>
+    </ParallaxSection>
+  );
 };
